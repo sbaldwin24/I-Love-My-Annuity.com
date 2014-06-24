06bcb60bd100b9695611c70a766b11f70efac359
Sterling Baldwin <sterling.baldwin@gmail.com>
2014-06-24 15:52:13 -0500

    V1 of I-Love-My-Annuity.com includes extra information about FIAs

---
+++ scripts/contact.php
@@ -0,0 +1,9 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if(!empty($_POST['contactname']) && !empty($_POST['contactemail']) && !empty($_POST['contactphone']) && !empty($_POST['contactzip']) && !empty($_POST['contactage']) && !empty($_POST['contactinvestment'])) {
+	$to = 'sterling.baldwin@gmail.com'; // Your e-mail address here.
+	$body = "\nName: {$_POST['contactname']}\nEmail: {$_POST['contactemail']}\n\n\n{$_POST['contactphone']}\n\n";
+	mail($to, "Lead from ILoveMyAnnuity.com", $body, "From: {$_POST['contactemail']}"); // E-Mail subject here.
+    }
+}
+?>
---
+++ scripts/custom.js
@@ -0,0 +1,197 @@
+//------------------------------------------------------------------------
+//								PRELOADER SCRIPT
+//------------------------------------------------------------------------
+$(window).load(function() { // makes sure the whole site is loaded
+
+	"use strict";
+    
+    $('#preloader').delay(400).fadeOut('slow'); // will fade out the white DIV that covers the website.
+    $('.clock').fadeOut(); // will first fade out the loading animation
+	
+	new WOW().init();
+	
+	$.stellar();
+	
+})
+
+
+
+
+$(document).ready(function(){
+	
+	"use strict";	
+	
+//------------------------------------------------------------------------
+//						TESTIMONIALS SLIDER SETTINGS
+//------------------------------------------------------------------------
+    var owl = $("#testimonials-slider");
+    owl.owlCarousel({
+        items : 5, 
+        itemsDesktop : [1400,4], 
+        itemsDesktopSmall : [1200,3], 
+        itemsTablet: [900,2], 
+        itemsMobile : [600,1],
+		autoPlay : 4000,
+		stopOnHover:true
+    });
+	
+	
+	 
+	 
+//------------------------------------------------------------------------
+//						INTRO SUPERSLIDER SETTINGS
+//------------------------------------------------------------------------
+	$("#slides").superslides({
+		play: 8000,
+		animation: "fade",
+		pagination: false,
+		inherit_height_from:"#intro"
+	});
+		
+
+
+	
+//------------------------------------------------------------------------
+//					SUBSCRIBE FORM VALIDATION'S SETTINGS
+//------------------------------------------------------------------------          
+    $('#subscribe_form').validate({
+        onfocusout: false,
+        onkeyup: false,
+        rules: {
+            email: {
+                required: true,
+                email: true
+            }
+        },
+        errorPlacement: function(error, element) {
+            error.appendTo( element.closest("form"));
+        },
+        messages: {
+            email: {
+                required: "We need your email address to contact you",
+                email: "Please, enter a valid email"
+            }
+        },
+					
+        highlight: function(element) {
+            $(element)
+        },                    
+					
+        success: function(element) {
+            element
+            .text('').addClass('valid')
+        }
+    }); 
+	
+
+		
+				
+//------------------------------------------------------------------------------------
+//						SUBSCRIBE FORM MAILCHIMP INTEGRATIONS SCRIPT
+//------------------------------------------------------------------------------------		
+    $('#subscribe_form').submit(function() {
+        $('.error').hide();
+        $('.error').fadeIn();
+        // submit the form
+        if($(this).valid()){
+            $('#subscribe_submit').button('loading'); 
+            var action = $(this).attr('action');
+            $.ajax({
+                url: action,
+                type: 'POST',
+                data: {
+                    newsletter_email: $('#subscribe_email').val()
+                },
+                success: function(data) {
+                    $('#subscribe_submit').button('reset');
+                    $('.error').html(data);
+                },
+                error: function() {
+                    $('#subscribe_submit').button('reset');
+                    // Change subscribe form error message text here
+                    $('.error').html('Oops! Something went wrong!');
+                }
+            });
+        // return false to prevent normal browser submit and page navigation 
+        }
+        return false; 
+    });
+	  
+	
+
+
+	
+//------------------------------------------------------------------------------------
+//						CONTACT FORM VALIDATION'S SETTINGS
+//------------------------------------------------------------------------------------		  
+    $('#contact_form').validate({
+        onfocusout: false,
+        onkeyup: false,
+        rules: {
+            name: "required",
+            message: "required",
+            email: {
+                required: true,
+                email: true
+            }
+        },
+        errorPlacement: function(error, element) {
+            error.insertBefore(element);
+        },
+        messages: {
+            name: "What's your name?",
+            message: "Type your message",
+            email: {
+                required: "What's your email?",
+                email: "Please, enter a valid email"
+            }
+        },
+					
+        highlight: function(element) {
+            $(element)
+            .text('').addClass('error')
+        },                    
+					
+        success: function(element) {
+            element
+            .text('').addClass('valid')
+        }
+    });   
+
+
+
+
+//------------------------------------------------------------------------------------
+//								CONTACT FORM SCRIPT
+//------------------------------------------------------------------------------------	
+	
+    $('#contact_form').submit(function() {
+        // submit the form
+        if($(this).valid()){
+            $('#contact_submit').button('loading'); 
+            var action = $(this).attr('action');
+            $.ajax({
+                url: action,
+                type: 'POST',
+                data: {
+                    contactname: $('#contact_name').val(),
+                    contactemail: $('#contact_email').val(),
+                    contactmessage: $('#contact_message').val()
+                },
+                success: function() {
+                    $('#contact_submit').button('reset');
+                    $('#contact_submit').button('complete');
+                },
+                error: function() {
+                    $('#contact_submit').button('reset');
+                    $('#contact_submit').button('error');
+                }
+            });
+        // return false to prevent normal browser submit and page navigation 
+        } else {
+            $('#contact_submit').button('reset')
+        }
+        return false; 
+    });	        
+
+});
