4f28a703eb540570936f61c46eb8e5cffe92c4cd
Sterling Baldwin <sterling.baldwin@gmail.com>
2014-08-08 16:25:12 -0500

    Experimenting with Logo

--- scripts/custom.js
+++ scripts/custom.js
@@ -51,6 +51,39 @@ $(document).ready(function(){
 	});
 		
 
+(function( $ ){
+
+  $.fn.fitText = function( kompressor, options ) {
+
+    // Setup options
+    var compressor = kompressor || 1,
+        settings = $.extend({
+          'minFontSize' : Number.NEGATIVE_INFINITY,
+          'maxFontSize' : Number.POSITIVE_INFINITY
+        }, options);
+
+    return this.each(function(){
+
+      // Store the object
+      var $this = $(this);
+
+      // Resizer() resizes items based on the object width divided by the compressor * 10
+      var resizer = function () {
+        $this.css('font-size', Math.max(Math.min($this.width() / (compressor*3), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
+      };
+
+      // Call once to set.
+      resizer();
+
+      // Call on resize. Opera debounces their resize by default.
+      $(window).on('resize.fittext orientationchange.fittext', resizer);
+
+    });
+
+  };
+
+})( jQuery );
+jQuery("#Liisa").fitText()
 
 	
 //------------------------------------------------------------------------
